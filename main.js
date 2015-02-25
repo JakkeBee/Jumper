381fc4b6849b540eb13eb12b24936647f64c039a
JakkeBee <janbruyninckx@MacBook-Pro-van-Jan.local>
2015-02-25 11:14:56 +0100

    Touch final change

--- main.js
+++ main.js
@@ -30,10 +30,10 @@ var mainState = {
 
         game.physics.arcade.enable(this.player);
         this.player.body.setSize(20, 20, 0, 0);
-        this.player.body.gravity.y = 1000;       
+        this.player.body.gravity.y = 1000;  
         
-        game.input.onDown.add(this.jump, this);
-        
+        game.input.onTap.add(this.jump, this);
+            
         var spaceKey = this.game.input.keyboard.addKey(Phaser.K);
         spaceKey.onDown.add(this.jump, this);
         
